e983ac397fb670eea744c84ffdfb31c4ad0bdfa9
akshshu <akshshu@gmail.com>
2020-03-25 23:17:57 +0530

    leaderboard added

--- snake.py
+++ snake.py
@@ -1,34 +1,7 @@
 import curses
 import random
-import time
 from curses import textpad
-'''import curses'''
-
-
-'''def main(scr):
-    curses.curs_set(0)
-    scr.addstr(15, 5, "hello")  # y cor,xcor,print
-    scr.refresh()
-    time.sleep(3)'''
-
-
-'''def main(scr):
-    h, w = scr.getmaxyx()  # to get the hegiht and width of screen
-    text = "hello world"
-    # foreground,background
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-
-    x = w//2-int(len(text)//2)
-    y = h//2
-    scr.attron(curses.color_pair(1))
-
-    scr.addstr(y, x, text)
-    scr.attroff(curses.color_pair(1))
-
-    scr.refresh()
-    time.sleep(2)'''
-
-menu = ['HOME', 'PLAY', 'SCORE', 'EXIT']
+menu = ['PLAY', 'LEADERBOARD', 'EXIT']
 
 
 def print_menu(screen, selected_row_idx):
@@ -46,6 +19,25 @@ def print_menu(screen, selected_row_idx):
     screen.refresh()
 
 
+def leader(screen):
+    screen.clear()
+    f2 = open('k.txt')
+    lis = f2.readlines()
+    f2.close()
+    sk = []
+    for i in range(len(lis)):
+        sc = lis[i].split(";")
+        sc[1] = sc[1].strip()
+        sk.append(sc)
+    sk.sort(key=lambda x: x[1])
+    for i in range(len(lis)):
+        screen.addstr(3+i, 3, sk[i][0])
+        screen.addstr(3+i, 30, sk[i][1])
+        screen.refresh()
+    print(sk)
+    screen.getch()
+
+
 def create_food(snake, w, h):
     food = None
     while food is None:
@@ -54,19 +46,24 @@ def create_food(snake, w, h):
         food = [food_x, food_y]
         if food in snake:
             food = None
-        return food
+    return list(food)
 
 
 def player(screen, h, w):
     screen.addstr(h//2, w//2, "Enter Player's Name")
     curses.echo()
     screen.move(h//2+1, w//2)
-    string = screen.getstr()
-    return string
+    p = screen.getstr().decode('utf8')
+    return p
+
+
+def disp_score(screen, score):
+    screen.addstr(1, 1, "SCORE :"+str(score))
 
 
 def snake(screen, name):
     score = 0
+    disp_score(screen, score)
     h, w = screen.getmaxyx()
     curses.curs_set(0)
     screen.clear()
@@ -111,6 +108,7 @@ def snake(screen, name):
         if(snake[0] == food):
             food = create_food(snake, w, h)
             score += 1
+            disp_score(screen, score)
             screen.addch(food[1], food[0], '*')
         else:
             screen.addch(snake[-1][1], snake[-1][0], ' ')
@@ -119,10 +117,15 @@ def snake(screen, name):
            box[0][1] == new_head[1]or box[1][0] == new_head[1]or
            snake[0]in snake[1:]):
             screen.addstr(h//2, w//2, "game over")
-            f = open('k.txt', 'ab')
-            f.write(name.encode())
+            f = open('k.txt', 'a+')
+            non_emp = f.read(2)
+            if len(non_emp) > 0:
+                f.write("\n")
+            f.write(name+";"+str(score))
+            f.write("\n")
+            f.close()
             screen.nodelay(0)
-            screen.getch()
+            screen.timeout(10000)
             break
 
 
@@ -144,7 +147,10 @@ def main(screen):
         if(key == curses.KEY_ENTER or key in[10, 13]):
             if(current_row_idx == len(menu)-1):
                 break
-            snake(screen, name)
+            elif(current_row_idx == 0):
+                snake(screen, name)
+            elif(current_row_idx == 1):
+                leader(screen)
         print_menu(screen, current_row_idx)
     screen.refresh()
 
